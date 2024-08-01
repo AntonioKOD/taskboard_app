a816810e5edc4ccc6aa59e7a2513e3abdcebbf81
AntonioKOD <antonio_kodheli@icloud.com>
2024-07-31 22:01:13 -0400

    add new file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+let nextId = JSON.parse(localStorage.getItem("nextId")) || 1;
 
 
 
